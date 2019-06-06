5a2fd7bcee9cb8efb4c6ecca137a7b3ca3c29193
stack72 <public@paulstack.co.uk>
2019-06-06 17:32:00 +0300

    Addition of a simple NewRelic example

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -1,95 +1,60 @@
-// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.(
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package examples
 
 import (
-	"io/ioutil"
-	"net/http"
+	"os"
+	"path"
 	"testing"
-	"time"
-
-	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/testing/integration"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestExamples(t *testing.T) {
-	// Ensure we have any required configuration points
-	// region := os.Getenv("AWS_REGION")
-	// if region == "" {
-	// 	t.Skipf("Skipping test due to missing AWS_REGION environment variable")
-	// }
-	// cwd, err := os.Getwd()
-	// if !assert.NoError(t, err, "expected a valid working directory: %v", err) {
-	// 	return
-	// }
-
-	// base options shared amongst all tests.
-	//base := integration.ProgramTestOptions{
-	//Config: map[string]string{
-	//// Configuration map
-	//},
-	//Tracing: "https://tracing.pulumi-engineering.com/collector/api/v1/spans",
-	//}
-	//baseJS := base.With(integration.ProgramTestOptions{
-	//Dependencies: []string{
-	//// JavaScript dependencies
-	//},
-	//})
-
-	examples := []integration.ProgramTestOptions{
-		// List each test
-		// baseJS.With(integration.ProgramTestOptions{
-		// 	Dir: path.Join(cwd, "api"),
-		// 	ExtraRuntimeValidation: validateAPITest(func(body string) {
-		// 		assert.Equal(t, "Hello, world!", body)
-		// 	}),
-		// 	EditDirs: []integration.EditDir{{
-		// 		Dir:      "./api/step2",
-		// 		Additive: true,
-		// 		ExtraRuntimeValidation: validateAPITest(func(body string) {
-		// 			assert.Equal(t, "<h1>Hello world!</h1>", body)
-		// 		}),
-		// 	}},
-		// 	ExpectRefreshChanges: true,
-		// }),
+func TestDomain(t *testing.T) {
+	token := os.Getenv("NEWRELIC_API_KEY")
+	if token == "" {
+		t.Skipf("Skipping test due to missing NEWRELIC_API_KEY environment variable")
 	}
 
-	if !testing.Short() {
-		// Append any longer running tests
+	cwd, err := os.Getwd()
+	if !assert.NoError(t, err) {
+		t.FailNow()
 	}
 
-	for _, ex := range examples {
+	var base = integration.ProgramTestOptions{
+		ExpectRefreshChanges: true,
+		Config: map[string]string{
+			"newrelic:api_key": token,
+		},
+	}
+
+	baseJS := base.With(integration.ProgramTestOptions{
+		Dependencies: []string{
+			"@pulumi/newrelic",
+		},
+	})
+
+	tests := []integration.ProgramTestOptions{
+		baseJS.With(integration.ProgramTestOptions{Dir: path.Join(cwd, "alertpolicy")}),
+	}
+
+	for _, ex := range tests {
 		example := ex
 		t.Run(example.Dir, func(t *testing.T) {
 			integration.ProgramTest(t, &example)
 		})
-	}
-}
-
-func createEditDir(dir string) integration.EditDir {
-	return integration.EditDir{Dir: dir, ExtraRuntimeValidation: nil}
-}
-
-func validateAPITest(isValid func(body string)) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-		var resp *http.Response
-		var err error
-		url := stack.Outputs["url"].(string)
-		// Retry a couple times on 5xx
-		for i := 0; i < 2; i++ {
-			resp, err = http.Get(url + "/b")
-			if !assert.NoError(t, err) {
-				return
-			}
-			if resp.StatusCode < 500 {
-				break
-			}
-			time.Sleep(10 * time.Second)
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		assert.NoError(t, err)
-		isValid(string(body))
 	}
 }
