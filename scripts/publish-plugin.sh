d313881e1a1ff1a6030d3f2485be4b795c307d6d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-01-17 13:58:18 -0800

    Test

--- scripts/publish-plugin.sh
+++ scripts/publish-plugin.sh
@@ -3,13 +3,10 @@
 # s3://rel.pulumi.com/releases/plugins.
 set -o nounset -o errexit -o pipefail
 
-# Update this with the name of the provider
-PROVIDER_NAME="newrelic"
-
 ROOT=$(dirname $0)/..
 WORK_PATH=$(mktemp -d)
 VERSION=$(jq -r '.version' < "${ROOT}/sdk/nodejs/bin/package.json")
-PLUGIN_PACKAGE_NAME="pulumi-resource-${PROVIDER_NAME}-${VERSION}-$(go env GOOS)-$(go env GOARCH).tar.gz"
+PLUGIN_PACKAGE_NAME="pulumi-resource-newrelic-${VERSION}-$(go env GOOS)-$(go env GOARCH).tar.gz"
 PLUGIN_PACKAGE_DIR="$(mktemp -d)"
 PLUGIN_PACKAGE_PATH="${PLUGIN_PACKAGE_DIR}/${PLUGIN_PACKAGE_NAME}"
 
@@ -20,9 +17,9 @@ if [ "$(go env GOOS)" = "windows" ]; then
 fi
 
 go build \
-   -ldflags "-X github.com/pulumi/pulumi-${PROVIDER_NAME}/pkg/version.Version=${VERSION}" \
-   -o "${WORK_PATH}/pulumi-resource-${PROVIDER_NAME}${BIN_SUFFIX}" \
-   "${ROOT}/cmd/pulumi-resource-${PROVIDER_NAME}"
+   -ldflags "-X github.com/pulumi/pulumi-newrelic/pkg/version.Version=${VERSION}" \
+   -o "${WORK_PATH}/pulumi-resource-newrelic${BIN_SUFFIX}" \
+   "${ROOT}/cmd/pulumi-resource-newrelic"
 
 # Tar up the plugin
 tar -czf ${PLUGIN_PACKAGE_PATH} -C ${WORK_PATH} .
